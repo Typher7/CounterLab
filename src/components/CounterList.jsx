95f457dc9b5b671c64a35fcc1f5d1995d1003b69
Enoch Atuahene <enatua01@wsc.edu>
2025-09-24 23:05:25 -0500

    added styling

--- src/components/CounterList.jsx
+++ src/components/CounterList.jsx
@@ -2,7 +2,7 @@ import CounterCard from "./CounterCard";
 
 function CounterList({ counters, onUpdate, onReset, onDelete }) {
   return (
-    <div className="counters">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
       {counters.map((c) => (
         <CounterCard
           key={c.id}
